ecb1502f69f3c5b199c95357e55b94dfddb00d99
dung <noajbarker@icloud.com>
2024-09-28 17:32:32 +0100

    added suffix stuff and len

--- trie.py
+++ trie.py
@@ -5,28 +5,22 @@ class Trie:
     repeating.
 
     As only characters are used, other datatypes can store extra info. 
-     - 0 distinguishes the end of a word
-     - 1 stores a count 
     """
-    dictionary = {}
+    __dict = {}
 
-    def __init__(self, strings):
+    def __init__(self, strings=None):
         """ Initialise from list of strings """
-        for string in strings:
-            children = self.dictionary # start at first layer of tree
-            for letter in string:
-                if letter not in children: 
-                    children[letter] = {} # `letter` becomes a child node
-                children = children[letter] # traverse to next layer
-            children[0] = 0 # use to store end of a word
+        if strings: 
+            self += strings
 
     def __contains__(self, string):
         """ Return whether word is `in` the tree """
-        children = self.dictionary
+        children = self.__dict
         for letter in string:
-            if letter not in children: return False
+            if letter not in children: 
+                return False
             children = children[letter]
-        return 0 in children
+        return "end" in children
 
     def __call__(self, string):
         self.__search_string = string
@@ -34,7 +28,7 @@ class Trie:
 
     def __iter__(self):
         """ Iterates over a string and returns the entire tree below """
-        children = self.dictionary 
+        children = self.__dict 
         for letter in self.__search_string:
             if letter not in children:
                 yield None
@@ -44,12 +38,15 @@ class Trie:
 
     def append(self, string):
         """ Add a single string to the trie """
-        children = self.dictionary 
+        children = self.__dict # start at first layer of tree
         for letter in string:
             if letter not in children: 
-                children[letter] = {}
-            children = children[letter]
-        children[0] = 0
+                children[letter] = {"counts": 1} # `letter` becomes a child node
+            else:
+                children[letter]["counts"] += 1 # update count
+
+            children = children[letter] # traverse to next layer
+        children["end"] = True # use to store end of a word
 
     def __add__(self, strings):
         for string in strings:
@@ -58,19 +55,28 @@ class Trie:
 
     def __from_dict(self, dictionary):
         trie = Trie([])
-        trie.dictionary = dictionary
+        trie.__dict = dictionary
         return trie
 
     def suffixes(self, string):
         """ Find child nodes (suffixes) of a string """
-        children = self.dictionary
+        children = self.__dict
         for letter in string:
-            if letter not in string: return None
+            if letter not in children: 
+                return None
             children = children[letter]
 
         return self.__from_dict(children) # convert to trie to keep methods
 
     def __repr__(self):
-        return f"Trie{self.dictionary}"
+        return f"Trie{self.__dict}"
 
-trie = Trie(["cat", "cot", "comb", "combs", "dog", "dogs"])
+    def __len__(self):
+        return self.__dict.get("counts") or len(self.__dict) 
+
+trie = Trie()
+l = ["cat", "car", "cot", "comb", "combs", "dog", "dogs"]
+trie = Trie(l)
+
+for children in trie("cat"):
+    print( len(children))
