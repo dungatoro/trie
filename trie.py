6da90001c2efdebd2bf58a2d57c076ca8635e8f9
dung <noajbarker@icloud.com>
2024-09-28 22:44:26 +0100

    switched back to dict approach and golfed

--- trie.py
+++ trie.py
@@ -1,22 +1,31 @@
 class Trie:
-    def __init__(self, *words): self.nodes, self.count = {}, 1; self += words
-
-    def append(self, word): 
-        curr = self
-        for i, letter in enumerate(word):
-            if letter not in curr.nodes:
-                curr.nodes[letter] = Trie(word[i+1:])
-                return
-            curr = curr.nodes[letter]; curr.count += 1
-        curr.end = True
+    def __init__(self, words): self.__d = {1: 0}; self += words
 
     def __add__(self, words):
         for word in words: self.append(word)
         return self
 
+    def __contains__(self, word, not_in_func, after_func):
+        nodes = self.__d
+        for char in word:
+            if char not in nodes: return False
+            nodes = nodes[char]
+        return 0 in x # {0: True} marks an end of word
+
+    def __len__(self): return self.__d[1]
+    def __repr__(self): return f"Trie{self.__d}"
+
+    def append(self, word):
+        self.__d[1], nodes = self.__d[1]+1, self.__d
+        for char in word:
+            if char not in nodes: nodes[char]     = {1: 1} 
+            else:                 nodes[char][1] += 1
+            nodes = nodes[char]
+        nodes[0] = True
+
     def counts(self, word):
-        curr, end = self, False
-        for letter in word:
-            if end == True: yield 0
-            elif letter not in curr.nodes: end = True; yield 0
-            else: curr = curr.nodes[letter]; yield curr.count
+        nodes, end = self.__d, False
+        for char in word:
+            if end:                                    yield 0
+            if char not in nodes: nodes = [];          yield 0
+            else:                 nodes = nodes[char]; yield nodes[1]
